59e6b429e723d5daee654dded6868e71ff71445f
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-07-24 14:17:51 +0300

    Updates

---
+++ precache-manifest.43490ad6847f12f549ba18bcc3fa380f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac3af4daab1ea879706a8f3b914823e",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "0dc507a7ea36024aef8c",
+    "url": "/react_add-todo-form/static/css/main.7a4a5691.chunk.css"
+  },
+  {
+    "revision": "9d1094e3d6f8817a0fe4",
+    "url": "/react_add-todo-form/static/js/2.d9d1bc8f.chunk.js"
+  },
+  {
+    "revision": "0dc507a7ea36024aef8c",
+    "url": "/react_add-todo-form/static/js/main.94125c39.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
